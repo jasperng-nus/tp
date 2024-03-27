618b74caf9e0d5e6cdded01c23ed678bb7a71eaf
Isaac <isaacong1@hotmail.com>
2024-03-27 14:11:46 +0800

    Add Date class

---
+++ src/main/java/seedu/address/model/person/Date.java
@@ -0,0 +1,61 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+/**
+ * Represents a Company's application date in the inter book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDate(String)}
+ */
+public class Date {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Date should be in the format YYYY-MM-DD";
+    public final LocalDate date;
+
+    /**
+     * Constructs a {@code Date}.
+     *
+     * @param date A valid date.
+     */
+    public Date(String date) {
+        requireNonNull(date);
+        checkArgument(isValidDate(date), MESSAGE_CONSTRAINTS);
+        this.date = LocalDate.parse(date);
+    }
+
+    /**
+     * Returns true if given string is a valid date.
+     */
+    public static boolean isValidDate(String test) {
+        try {
+            LocalDate.parse(test);
+            return true;
+        } catch (DateTimeException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return date.toString();
+    }
+
+    //Might not be used, delete later.
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Date)) {
+            return false;
+        }
+
+        Date otherDate = (Date) other;
+        return date.equals(otherDate.date);
+    }
+}

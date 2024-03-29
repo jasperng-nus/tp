51e206779faeffed4b75777be66c83e49bc51124
AjayArvind2207 <alwayssactheexchange@gmail.com>
2024-03-30 00:02:44 +0800

    Fix checkstyle issues related to wild card imports

--- src/test/java/seedu/address/model/company/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/company/NameContainsKeywordsPredicateTest.java
@@ -1,11 +1,13 @@
 package seedu.address.model.company;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.testutil.CompanyBuilder;
-
 
 
 public class NameContainsKeywordsPredicateTest {

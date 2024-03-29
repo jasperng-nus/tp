42681760085de39ce13b3608919ee19e0d383f62
jasperng-nus <110957844+jasperng-nus@users.noreply.github.com>
2024-03-29 20:43:28 +0800

    bug fix: change order of import statements

--- src/test/java/seedu/address/logic/commands/MarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MarkCommandTest.java
@@ -13,10 +13,10 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
-import seedu.address.model.company.Company;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.company.Company;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for

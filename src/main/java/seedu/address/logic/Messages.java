431cf649d2aa3aec1d3602776efd8a64564f2fa7
cocoanautz <55192178+cocoanautz@users.noreply.github.com>
2024-04-04 23:33:41 +0800

    Update Messages.java

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,7 +14,7 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_STARTDATE = "Start Date must be earlier than End Date";
+    public static final String MESSAGE_INVALID_DATERANGE = "Start Date must be earlier than End Date";
     public static final String MESSAGE_INVALID_COMPANY_DISPLAYED_INDEX = "The company index provided is invalid";
     public static final String MESSAGE_COMPANIES_LISTED_OVERVIEW = "%1$d companies listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =

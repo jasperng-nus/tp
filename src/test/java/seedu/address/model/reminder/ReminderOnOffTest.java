5df4a927d06739d85066044e5e9fce625f41b181
jasperng-nus <e0959074@u.nus.edu>
2024-04-15 00:36:59 +0800

    Add ReminderOnOffTest to test the methods in ReminderOnOff class

---
+++ src/test/java/seedu/address/model/reminder/ReminderOnOffTest.java
@@ -0,0 +1,76 @@
+package seedu.address.model.reminder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ReminderOnOffTest {
+
+    @Test
+    void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new ReminderOnOff(null));
+    }
+
+    @Test
+    void constructor_emptySetReminder_throwsIllegalArgumentException() {
+        String emptySetReminder = "";
+        assertThrows(IllegalArgumentException.class, () -> new ReminderOnOff(emptySetReminder));
+    }
+
+    @Test
+    void constructor_invalidSetReminder_throwsIllegalArgumentException() {
+        String invalidSetReminder = "truee";
+        assertThrows(IllegalArgumentException.class, () -> new ReminderOnOff(invalidSetReminder));
+    }
+
+    @Test
+    void constructor_defaultReminder() {
+        // default reminder is false
+        ReminderOnOff defaultReminder = new ReminderOnOff();
+        assertFalse(defaultReminder.getReminder());
+    }
+
+    @Test
+    void isValidSetReminder() {
+        // invalid set reminder
+        assertFalse(ReminderOnOff.isValidSetReminder("")); // empty string
+        assertFalse(ReminderOnOff.isValidSetReminder(" ")); // spaces only
+        assertFalse(ReminderOnOff.isValidSetReminder("truee")); // incorrect spelling
+        assertFalse(ReminderOnOff.isValidSetReminder("falsee")); // incorrect spelling
+        assertFalse(ReminderOnOff.isValidSetReminder("phone")); // non-boolean
+        assertFalse(ReminderOnOff.isValidSetReminder("1")); // non-boolean
+
+        // valid set reminder
+        assertTrue(ReminderOnOff.isValidSetReminder("true")); // true
+        assertTrue(ReminderOnOff.isValidSetReminder("false")); // false
+    }
+
+    @Test
+    void getReminder() {
+        // default reminder is false
+        ReminderOnOff reminderOnOff = new ReminderOnOff();
+        assertFalse(reminderOnOff.getReminder());
+
+        // set reminder to true
+        reminderOnOff = new ReminderOnOff("true");
+        assertTrue(reminderOnOff.getReminder());
+
+        // set reminder to false
+        reminderOnOff = new ReminderOnOff("false");
+        assertFalse(reminderOnOff.getReminder());
+    }
+
+    @Test
+    void testToString() {
+        // set reminder to true
+        ReminderOnOff reminderOnOff = new ReminderOnOff("true");
+        assertEquals("Reminder is turned on", reminderOnOff.toString());
+
+        // set reminder to false
+        reminderOnOff = new ReminderOnOff("false");
+        assertEquals("Reminder is turned off", reminderOnOff.toString());
+    }
+}
